65e4f60dc31274c9d1546fecbce24612cc72d399
DESKTOP-IM9483I\Uri <loiro_8@hotmail.com>
2022-09-14 15:36:37 +0200

    First commit

---
+++ src/test/java/com/uriart/msa/MicroStreamingAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uriart.msa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroStreamingAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
